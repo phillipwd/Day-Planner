4515bed34e41e451d8e2fa4dc3b9029a7d9880d6
Phillip Dunham <phillipwwd@gmail.com>
2019-12-11 22:16:26 -0800

    Clock works in header

--- scripts.js
+++ scripts.js
@@ -2,8 +2,8 @@ var input = document.getElementsByTagName("textarea");
 console.log(input[0].textContent);// works
 var save = document.getElementsByClassName("saveButton");
 var Time = document.getElementsByClassName("lead");
-var now = moment();
-Time[0].innerHTML = now;
+
+
 
 
 input[0].textContent = localStorage.getItem("8a")
@@ -16,3 +16,7 @@ $(save[0]).on("click", function(){
     localStorage.setItem("8a", s1);    
 });
 
+var setTime = function (){
+    Time[1].innerHTML = moment().format("MMMM Do YYYY, h:mm:ss a");
+}
+setInterval(setTime,1000);
